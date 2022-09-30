6f572ecaa36a2bc1d8b4ea0e6bfba573723acb47
ISMAEL HURTADO MARQUES <ismael.hmarques@senacsp.edu.br>
2022-09-29 21:47:41 -0300

    Criacao do modelo do aluno

---
+++ CadastroAluno/Models/Aluno.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroAluno.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Turma { get; set; }
+        public double Media { get; set; }
+
+        public void AtualizarDados(string nome, string turma)
+        {
+            Nome = nome;
+            Turma = turma;
+            
+        }
+
+        public bool VerificaAprovação()
+        {
+            if (Media > 5)
+            {
+                return true;
+            }
+
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
