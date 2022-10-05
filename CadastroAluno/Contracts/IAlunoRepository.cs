bd0dd1418c7f0f8fae0dffb59625203db0ae58fd
ISMAEL HURTADO MARQUES <ismael.hmarques@senacsp.edu.br>
2022-10-04 22:13:35 -0300

    Alunos Models

---
+++ CadastroAluno/Contracts/IAlunoRepository.cs
@@ -0,0 +1,17 @@
+﻿using CadastroAluno.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroAluno.Contracts
+{
+    public interface IAlunoRepository
+    {
+        Task<List<Aluno>> Index();
+        Task<Aluno> Details(int id);
+        Task<Aluno> Create(Aluno cliente);
+        Task<Aluno> Edit(int id, Aluno cliente);
+        Task Delete(int id);
+    }
+}
---
+++ CadastroAluno/Repository/AlunoRepositoy.cs
@@ -0,0 +1,48 @@
+﻿using CadastroAluno.Contracts;
+using CadastroAluno.Data;
+using CadastroAluno.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroAluno.Repository
+{
+    public class AlunoRepository : IAlunoRepository
+    {
+        private readonly CadastroAlunoContext _context;
+
+        public AlunoRepository(CadastroAlunoContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Aluno>> Index()
+        {
+            return await _context.Aluno.ToListAsync();
+        }
+        public async Task<Aluno> Details(int id)
+        {
+            return await _context.Aluno.FindAsync(id);
+        }
+        public async Task<Aluno> Create(Aluno aluno)
+        {
+            await _context.Aluno.AddAsync(aluno);
+            await _context.SaveChangesAsync();
+            return aluno;
+
+        }
+        public async Task<Aluno> Edit(int id, Aluno alunoAlterado)
+        {
+            _context.Entry(alunoAlterado).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return alunoAlterado;
+        }
+        public async Task Delete(int id)
+        {
+            var alunoRemovido = await _context.Aluno.FirstOrDefaultAsync(a => a.Id == id);
+            _context.Aluno.Remove(alunoRemovido);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
