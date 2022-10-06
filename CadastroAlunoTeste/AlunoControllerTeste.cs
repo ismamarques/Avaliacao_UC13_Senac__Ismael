f6ba571ab1f10f1ef043abe277f9fdf18d24177c
ISMAEL HURTADO MARQUES <ismael.hmarques@senacsp.edu.br>
2022-10-06 20:08:01 -0300

    testes AlunoController

--- CadastroAlunoTeste/AlunoControllerTeste.cs
+++ CadastroAlunoTeste/AlunoControllerTeste.cs
@@ -1,125 +1,155 @@
-﻿using System;
+﻿using CadastroAluno.Contracts;
+using CadastroAluno.Controllers;
+using CadastroAluno.Models;
+using CadastroAluno.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using CadastroAluno.Data;
-using CadastroAluno.Models;
-using CadastroAluno.Contracts;
-namespace CadastroAluno.Controllers
+using Xunit;
+
+namespace CadastroAlunoTest
 {
-    public class AlunosController : Controller
+    public class AlunoControllerTest
     {
-        private readonly IAlunoRepository _context;
-        public AlunosController(IAlunoRepository context)
+        Mock<IAlunoRepository> _repository;
+        Aluno alunoValido;
+        Aluno alunoValido2;
+
+        public AlunoControllerTest()
         {
-            _context = context;
+            _repository = new Mock<IAlunoRepository>();
+            alunoValido = new Aluno()
+            {
+                Id = 1,
+                Nome = "Nome",
+                Media = 8,
+                Turma = "Turma 1"
+            };
+            alunoValido2 = new Aluno()
+            {
+                Id = -2,
+                Nome = "",
+                Media = 11,
+                Turma = ""
+            };
         }
 
-        // retornar uma ViewResult
-        public IActionResult Index()
+        [Fact]
+        public void Executa_Acao_RetornaOkAction()
         {
-            return View(_context.Index());
+            AlunosController controller = new AlunosController(_repository.Object);
+            var result = controller.Index();
+            Assert.IsType<ViewResult>(result);
+        }
+        [Fact(DisplayName = "Chama uma vex Repo)"]
+
+        public void ChamaRepositorioUmaVez()
+        {
+            AlunosController controller = new AlunosController(_repository.Object);
+            controller.Index();
+            
+
+            _repository.Verify(repo => repo.Index(), Times.Once);
+        }
+        [Fact(DisplayName = "Não  existe o aluno NotFound")]
+        public void AlunoNaoExisteNotFound()
+        {
+
+            AlunosController controller = new AlunosController
+                (
+                     _repository.Object
+                );
+
+
+            _repository.Setup(repo => repo.Details(1)).Returns
+                (
+                     alunoValido
+                );
+
+            var consulta = controller.Create(2);
+            Assert.IsType<NotFoundResult>(consulta);
+        }
+        [Fact(DisplayName = "Aluno Não Existe BadRequest")]
+        public void AlunoNaoExiste_RetornBadRequest()
+        {
+            AlunosController controller = new AlunosController(_repository.Object);
+
+            var consulta = controller.Create(-1);
+            
+            Assert.IsType<BadRequestResult>
+                (
+                    consulta
+                );
+        }
+
+        [Fact(DisplayName = "Chama Repo 1 Vez")]
+        public void DetalhesChamaRepositorioUmaVez()
+        {
+            AlunosController controller = new AlunosController(_repository.Object);
+
+            _repository.Setup(repo => repo.Details(1));
+            controller.Create(1);
+           
+            _repository.Verify(repo => repo.Details(1), Times.Once);
+        }
+
+        [Fact(DisplayName = "Detalhes Retorna View Result")]
+        public void RetornaViewResul()
+
+        {
+            AlunosController controller = new AlunosController(_repository.Object);
+            _repository.Setup(repo => repo.Details(1)).Returns(alunoValido);
+            var result = controller.Create(1);
+            Assert.IsType<ViewResult>(result);
         }
 
 
-        // Retornar com detalhes
-        public IActionResult Details(int id)
+        [Fact(DisplayName = "Criar Retorna ViewResult")]
+        public void ExecutaAcao_RetornaViewResultSempre()
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
 
-            var aluno = _context.Details(id);
-            if (aluno == null)
-            {
-                return NotFound();
-            }
-            return View(aluno);
+            AlunosController controller = new AlunosController(_repository.Object);
+            _repository.Setup(repo => repo.Create(alunoValido)).Returns(alunoValido);
+            var result = controller.Create(alunoValido);
+            Assert.IsType<RedirectToActionResult>(result);
         }
 
 
-        public IActionResult Create()
-        {
-            return View();
+        [Fact(DisplayName = "[HttpPost] Create() ou RedirectToAction ")]
+        public void ValidaDados_RetornaResposta()
+        {   
+
+            AlunosController controller = new AlunosController(_repository.Object);
+            
+            _repository.Setup(repo => repo.Create(alunoValido)).Returns(alunoValido);
+            var result = controller.Create(alunoValido);
+            
+
+            _repository.Verify(repo => repo.Create(alunoValido), Times.Once);
+            Assert.IsType<RedirectToActionResult>(result);
+        }
+        [Fact(DisplayName = " Redireciona para Ação ")]
+        public void ValidaDados_RetornaResposta_NEgativo()
+
+        {   
+
+            AlunosController controller = new AlunosController(_repository.Object);
+            
+
+            _repository.Setup(repo => repo.Create(alunoValido2)).Returns(alunoValido2);
+            var result = controller.Create(alunoValido2);
+           
+
+
+            _repository.Verify(repo => repo.Create(alunoValido2), Times.Once);
+            Assert.IsType<ViewResult>(result);
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create([Bind("Id,Nome,Turma,Media")] Aluno aluno)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Create(aluno);
-                return RedirectToAction(nameof(Index));
-            }
-            return View(aluno);
-        }
 
-        public IActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var  aluno = _context.Details((int)id);
-            if (aluno == null)
-            {
-                return NotFound();
-            }
-            return View(aluno);
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(int id, [Bind("Id,Nome,Turma,Media")] Aluno aluno)
-        {
-            if (id != aluno.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                _context.Edit(id, aluno);
-                return RedirectToAction(nameof(Index));
-            }
-            return View(aluno);
-        }
-
-        public IActionResult Delete(int id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var aluno = _context.Details(id);
-            if (aluno == null)
-            {
-                return NotFound();
-            }
-            return View(aluno);
-        }
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeleteConfirmed(int id)
-        {
-            var aluno = _context.Details(id);
-            _context.Delete(id);
-            return RedirectToAction(nameof(Index));
-        }
-
-        private IActionResult AlunoExists(int id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-            return Ok();
-        }
     }
+
 }
